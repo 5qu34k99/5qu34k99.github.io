c22034b9a5f851607c8eaedf318313e5f4761776
5qu34k <19596050+5qu34k99@users.noreply.github.com>
2026-01-03 17:29:51 -0500

    remove slide.js

--- PGW/assets/js/slide.js
+++
@@ -1,7 +0,0 @@
-const containers = document.querySelectorAll('.container');
-
-containers.forEach(container => {
-  container.querySelector('.slider').addEventListener('input', (e) => {
-    container.style.setProperty('--position',   `${e.target.value}%`);
-  })
-})
